1cdd0dae3132cbde2d55b823e847584c6232d00d
Sivraj Sahu <sivapr@gmail.com>
2026-05-16 02:15:09 +0530

    fixed render deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,5 +17,9 @@ app.use("/api/loans", loanRoutes);
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 export default app;
